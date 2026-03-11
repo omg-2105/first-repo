8d15636fe64adc26857c25dfd481fa7f8cabfcff
Alex <example@gmail.com>
2026-03-11 20:06:12 +0200

    Initial commit may be add hash

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 print("first commit")
 print("Робота на сьогодні закінчена")
 print("Кухня готова, Гіт працює!")
+
+# class MyClass:
+#     def __init__(self):
+#         self.name = "Alice"
+#
+#     def greet(self):
+#         print("Hello, " + self.name)
+#
+# obj = MyClass()
+# print(dir(obj))  # Выводит список атрибутов и методов объекта 'obj'
+# print(dir())
+
+# print(hash("hello"))  # Возвращает хеш-значение строки "hello"
+# print(hash(42))       # Возвращает хеш-значение числа 42
+# print(hash((1, 2, 3)))  # Возвращает хеш-значение кортежа (1, 2, 3)

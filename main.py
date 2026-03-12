1ac2ff5b213a0a48d2f400fee80c81d9a45386d4
Alex <example@gmail.com>
2026-03-12 19:59:49 +0200

    Initial commit added lambda-func

--- main.py
+++ main.py
@@ -16,3 +16,27 @@ print("Кухня готова, Гіт працює!")
 # print(hash("hello"))  # Возвращает хеш-значение строки "hello"
 # print(hash(42))       # Возвращает хеш-значение числа 42
 # print(hash((1, 2, 3)))  # Возвращает хеш-значение кортежа (1, 2, 3)
+
+# number = 12
+# string = 'hello'
+# my_list = [12, 'hello']
+# my_set = (12, 'hello')
+#
+# print(id(number))
+# print(id(string))
+# print(id(my_list))
+# print(id(my_set))
+#
+# print(hash(number))
+# print(hash(string))
+# print(hash(my_list))
+# print(hash(my_set))
+#
+# print(dir(number))
+# print(dir(string))
+# print(dir(my_list))
+# print(dir(my_set))
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(even_numbers)  # Вывод: [2, 4, 6, 8, 10]

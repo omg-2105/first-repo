7a857452e1550bbd2f6f67be6fb360a09a9f897e
Alex <example@gmail.com>
2026-03-14 19:05:45 +0200

    Initial commit added yield

--- main.py
+++ main.py
@@ -44,3 +44,47 @@ print("Кухня готова, Гіт працює!")
 # words = ["banana", "apple", "cherry", "date"]
 # sorted_words = sorted(words, key=lambda x: len(x))
 # print(sorted_words)  # Вывод: ['date', 'apple', 'banana', 'cherry']
+
+# pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
+# pairs.sort(key=lambda pair: pair[1])
+# print(pairs)  # Вывод: [(4, 'four'), (1, 'one'), (3, 'three'), (2, 'two')]
+
+# chisla = [1, 2, 3]
+# kvadrat_chisla = list(map(lambda x: x ** 2, chisla))
+# print(kvadrat_chisla)
+
+# def make_filter(threshold):
+#     # threshold — це "поріг", який замикання запам'ятає
+#     def filter_func(value):
+#         # Повертає True, якщо значення більше за поріг
+#         return value > threshold
+#
+#     return filter_func
+#
+#
+# # Набір даних для тесту (наприклад, VDI знахідок або розміри плитки)
+# data = [10, 25, 45, 60, 75, 90, 100]
+#
+# # 1. Створюємо фільтр для "дрібних" значень (пропускає все, що більше 30)
+# filter_medium = make_filter(30)
+#
+# # 2. Створюємо фільтр для "великих" значень (пропускає все, що більше 80)
+# filter_high = make_filter(80)
+#
+# # Використовуємо фільтри
+# print(f"Початковий список: {data}")
+#
+# # Відфільтруємо список за допомогою вбудованої функції filter()
+# result_medium = list(filter(filter_medium, data))
+# result_high = list(filter(filter_high, data))
+#
+# print(f"Значення більше 30: {result_medium}")
+# print(f"Значення більше 80: {result_high}")
+
+# def large_range(n):
+#     for i in range(n):
+#         yield i
+#
+# for value in large_range(1000000):
+#     # Обрабатываем значения по одному
+#     print(value)

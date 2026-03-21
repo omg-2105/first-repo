8ef36b63bd0ff1b592ddc61cbd54c5914603225d
Alex <example@gmail.com>
2026-03-21 18:16:42 +0200

    Initial commit added балуюсь кодом

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("first commit")
-print("Робота на сьогодні закінчена")
-print("Кухня готова, Гіт працює!")
+# print("first commit")
+# print("Робота на сьогодні закінчена")
+# print("Кухня готова, Гіт працює!")
 
 # class MyClass:
 #     def __init__(self):
@@ -152,3 +152,194 @@ print("Кухня готова, Гіт працює!")
 #
 # # Виклич функцію нижче:
 # download_lesson()
+
+# import platform
+#
+# print("Operating System:", platform.system())
+# print("Node Name:", platform.node())
+# print("OS Release:", platform.release())
+# print("OS Version:", platform.version())
+# print("Machine:", platform.machine())
+# print("Processor:", platform.processor())
+# print("Architecture:", platform.architecture())
+# print("Python Version:", platform.python_version())
+# print("Python Compiler:", platform.python_compiler())
+
+# def maximun_reproduction(years_to_flood, children_per_woman, generation_gap):
+#     population = 2  # Адам і Єва
+#     year = 0
+#
+#     # Кожні 80 років (середній час до появи онуків) рахуємо приріст
+#     while year < years_to_flood:
+#         # Тільки половина населення (жінки) народжує дітей
+#         females = population / 2
+#         new_children = females * children_per_woman
+#
+#         population += new_children
+#         year += generation_gap
+#
+#         # Виводимо проміжні результати кожні 400 років
+#         if year % 400 == 0:
+#             print(f"Рік {year}: {int(population):,}")
+#
+#     return int(population)
+#
+#
+# # Налаштування: 1656 років, 50 дітей на жінку, покоління кожні 80 років
+# total_people = maximun_reproduction(1656, 50, 80)
+# print(f"\nЗагальна кількість людей до Потопу: {total_people:,}")
+
+# def realistic_biblical_population(years_limit):
+#     population = 2
+#     # Список, де ми будемо зберігати вік груп людей (спрощено)
+#     # Кожні 20 років народжується нова група
+#     people_groups = {0: 2}  # вік: кількість людей
+#
+#     for year in range(20, years_limit + 20, 20):
+#         new_borns = 0
+#         current_groups = list(people_groups.items())
+#
+#         for age, count in current_groups:
+#             # Якщо вік від 60 до 450 — вони народжують (по 1 дитині на пару за 20 років)
+#             if 60 <= age <= 450:
+#                 new_borns += count / 2 * 2  # Кожна пара дає 2 дітей за 20 років
+#
+#             # Старіння: додаємо 20 років
+#             people_groups[age + 20] = people_groups.pop(age)
+#
+#             # Смертність: якщо старше 930 років — видаляємо з розрахунку
+#             if age + 20 > 930:
+#                 people_groups.pop(age + 20)
+#
+#         people_groups[0] = new_borns
+#         population = sum(people_groups.values())
+#
+#         if year % 200 == 0 or year == years_limit:
+#             print(f"Рік {year:4}: {int(population):,}")
+#
+#     return int(population)
+#
+#
+#
+#
+# # Запускаємо до 1656 року
+# all_people = realistic_biblical_population(1656)
+#
+# earth_area = 149_000_000
+# population_per_square_km = all_people // earth_area
+# print(population_per_square_km) # 2998
+#
+# population_per_square_ga = population_per_square_km / 100
+# rounded_pop_p_s_ga = round(population_per_square_ga)
+# print(rounded_pop_p_s_ga)
+
+# quantity_apricots = 5
+# water = 100
+# water_per_apricot = water / quantity_apricots
+#
+# for i in range(1, quantity_apricots + 1):
+#     print(f"Apricot №{i} gived {water_per_apricot} liters of water.")
+
+# quantity_apricots = 5
+# water = 100
+# water_per_apricot = water / quantity_apricots
+#
+# print(f"\nПлан на сьогодні: дати кожному дереву по {water_per_apricot} л. води.\n")
+#
+# # Починаємо з 1, закінчуємо на кількості + 1
+# for i in range(1, quantity_apricots + 1):
+#     print(f"Дерево №{i} полито. Використано {water_per_apricot} літрів. ✅")
+#
+# print("\nРоботу завершено! Сад напоєний.")
+
+# total_harvest = 0
+# current_yield = 10
+# quantity_trees = 6
+#
+# for i in range(1, quantity_trees + 1):
+#     # 1. Додаємо врожай поточного дерева до загального кошика
+#     total_harvest = total_harvest + current_yield
+#
+#     print(f"Дерево №{i} дало {current_yield} кг. У кошику вже {total_harvest} кг.")
+#
+#     # 2. Збільшуємо врожай для НАСТУПНОГО дерева на 5 кг
+#     current_yield = current_yield + 5
+#
+# print(f"\nФінальний результат: Ти зібрав {total_harvest} кг абрикосів! 🎉")
+
+# curr_power_percents = 100
+# fall_power = 5
+# for i in range(1, 7):
+#     curr_power_percents = curr_power_percents - fall_power
+#     print(f"First hour{i} - {curr_power_percents}")
+#     fall_power += 2
+#
+# print(curr_power_percents)
+#
+# curr_power_percents = 100
+# fall_power = 5  # скільки втрачаємо зараз
+#
+# for i in range(1, 7):  # тепер точно 6 годин
+#     curr_power_percents = curr_power_percents - fall_power
+#     print(f"Hour {i}: Battery - {curr_power_percents}% (lost {fall_power}%)")
+#     # Ось тут магія: збільшуємо ВТРАТУ на майбутнє
+#     fall_power = fall_power + 2
+#
+# print(f"\nFinal charge: {curr_power_percents}%")
+
+# class Library: # клас Бібліотека
+#     books = ['a', 'b', 'c']  # список книг в бібліотеці
+#     @classmethod # декоратор який показує що робити в бібліотеці
+#     def add_book(cls, book):  # функція незрозуміло чому сіельес?
+#         cls.books.append(book)  # додає книгу
+#     def display_books(self):  # функція незрозуміло чому селф?
+#         print(self.books)  # виводить список книг у бібліотеці
+# info = Library()   # присвоюємо змінній інфо клас Бібліотека?
+# info.add_book('d')   # тут ясно додаємо книгу д
+# info.display_books()  # звертаємось до функції яка виводить список книг
+
+class Orchard:
+    total_trees = 0
+
+    @classmethod
+    def add_tree(cls, number):
+        cls.total_trees += number
+    def display_status(self):
+        print(f"У саду зараз: {self.total_trees} дерев")
+info = Orchard()
+info.add_tree(10)
+info.display_status()
+
+# class Orchard:
+#     total_trees = 0
+#
+#     @classmethod
+#     def add_tree(cls, numbers_range):
+#         # Проходимо циклом по кожному числу в range
+#         for n in numbers_range:
+#             cls.total_trees += 1  # Додаємо по одному дереву за кожен крок
+#
+#     def display_status(self):
+#         print(f"У саду зараз: {self.total_trees} дерев")
+#
+#
+# info = Orchard()
+# info.add_tree(range(1, 11))  # Тепер це спрацює!
+# info.display_status()
+
+trees = 0
+
+for i in range(7):
+    i += 1
+    print(f"Tree #{i} planted")
+    trees = trees + 1
+print(trees)
+
+trees_count = 0
+
+# Кажемо range почати з 1 і зупинитися перед 8 (тобто на 7)
+for i in range(1, 8):
+    print(f"Tree #{i} planted")
+    trees_count += 1
+
+print(f"Total trees: {trees_count}")
